f9d68740ef522381652ca91e2e763fd38ffc4593
Mi-aro-101 <miaroramananadraitsiory@gmail.com>
2024-01-26 23:30:03 +0530

    pushing target

--- src/main/java/org/autotrader/configuration/SecurityConfig.java
+++ src/main/java/org/autotrader/configuration/SecurityConfig.java
@@ -3,17 +3,12 @@
  */
 package org.autotrader.configuration;
 
-import org.autotrader.service.UserDetail;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
