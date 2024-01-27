343af03bc7874dcb09b28b0ce129d99f623ce094
Mi-aro-101 <miaroramananadraitsiory@gmail.com>
2024-01-28 00:17:53 +0530

    Favoriser une annonce

--- src/main/java/org/autotrader/model/Annonce.java
+++ src/main/java/org/autotrader/model/Annonce.java
@@ -4,7 +4,9 @@
  */
 package org.autotrader.model;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
 
 import jakarta.persistence.CascadeType;
@@ -55,7 +57,10 @@ public class Annonce {
 	double tarif;
 	
 	@Column(name="date_annonce")
-	LocalDateTime dateAnnonce;
+	LocalDate dateAnnonce;
+	
+	@Column(name="temps_annonce")
+	LocalTime tempsAnnonce;
 	
 	@Column(name="etat")
 	Integer etat;
@@ -115,8 +120,12 @@ public class Annonce {
 		return tarif;
 	}
 	
-	public LocalDateTime getDateAnnonce() {
+	public LocalDate getDateAnnonce() {
 		return dateAnnonce;
+	}
+	
+	public LocalTime getTempsAnnonce() {
+		return tempsAnnonce;
 	}
 	
 	public Integer getEtat() {
@@ -176,8 +185,12 @@ public class Annonce {
 		this.tarif = tarif;
 	}
 	
-	public void setDateAnnonce(LocalDateTime dateAnnonce) {
+	public void setDateAnnonce(LocalDate dateAnnonce) {
 		this.dateAnnonce = dateAnnonce;
+	}
+	
+	public void setTempsAnnonce(LocalTime tempsAnnonce) {
+		this.tempsAnnonce = tempsAnnonce;
 	}
 	
 	public void setEtat(Integer etat) {
--- src/main/java/org/autotrader/service/AnnonceService.java
+++ src/main/java/org/autotrader/service/AnnonceService.java
@@ -10,6 +10,8 @@ import org.autotrader.dto.AnnonceDto;
 import org.autotrader.model.Annonce;
 import org.autotrader.model.Carburant;
 import org.autotrader.model.CategorieVoiture;
+import org.autotrader.model.Favori;
+import org.autotrader.model.FavoriRepository;
 import org.autotrader.model.ModeleVoiture;
 import org.autotrader.model.Utilisateur;
 import org.autotrader.repository.AnnonceRepository;
@@ -48,10 +50,13 @@ public class AnnonceService {
 	ModeleVoitureRepository modeleVoitureRepository;
 	
 	@Autowired
+	FavoriRepository favoriRepository;
+	
+	@Autowired
 	PhotoService photoService;
 	
 	@Transactional
-	public ResponseEntity<?> save(AnnonceDto annonceDto, MultipartFile[] files)throws Exception{
+	public ResponseEntity<?> save(AnnonceDto annonceDto /*, MultipartFile[] files*/)throws Exception{
 		
 		// Ilay utilisateur alaina @ alalan'ny token fa ataoko static aloha eto
 		Utilisateur user = utilisateurRepository.findById(1).orElseThrow();
@@ -72,15 +77,19 @@ public class AnnonceService {
 		annonce.setCategorieVoiture(categorieVoiture);
 		annonce.setAnnee(annonceDto.getAnnee());
 		annonce.setDateAnnonce(null);
+		annonce.setTempsAnnonce(null);
 		annonce.setDescriptionAnnonce(annonceDto.getDescriptionAnnonce());
+		// (Etat = 5) <=> Annonce en attente de validation par l'admin
 		annonce.setEtat(5);
+		///////////////////////////////////////////////////////////
 		annonce.setKilometrage(annonceDto.getKilometrage());
 		annonce.setNombrePlace(annonceDto.getNombrePlace());
 		annonce.setNombrePorte(annonceDto.getNombrePorte());
 		annonce.setTarif(annonceDto.getTarif());
 		
 		annonceRepository.save(annonce);
-		photoService.savePhotos(files, annonce);
+		
+//		photoService.savePhotos(files, annonce);
 		
 		return new ResponseEntity<>("Insertion avec succes", HttpStatus.OK);
 		
@@ -91,6 +100,24 @@ public class AnnonceService {
 		annonceRepository.findAll().forEach(annonces::add);
 		
 		return new ResponseEntity<>(annonces, HttpStatus.OK);
+	}
+	
+	public ResponseEntity<?> favoriser(Integer idAnnonce)throws Exception{
+		Annonce annonce = annonceRepository.findById(idAnnonce).orElseThrow();
+		
+		// Utilisateur en token
+		Utilisateur user = utilisateurRepository.findById(1).orElseThrow();
+		///////////////////////
+		
+		Favori favori = new Favori();
+		favori.setIdFavori(null);
+		favori.setUtilisateur(user);
+		favori.setAnnonce(annonce);
+		
+		favoriRepository.save(favori);
+		
+		return new ResponseEntity<>("Annonce mis dans votre liste de favori", HttpStatus.OK);
+		
 	}
 	
 }
