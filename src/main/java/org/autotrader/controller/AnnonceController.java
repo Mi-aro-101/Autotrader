d5cf3e5040053eac3d1307f7e6340826706c322f
Mi-aro-101 <miaroramananadraitsiory@gmail.com>
2024-02-07 02:12:18 +0530

    asdfl

--- src/main/java/org/autotrader/controller/AnnonceController.java
+++ src/main/java/org/autotrader/controller/AnnonceController.java
@@ -24,7 +24,7 @@ import org.springframework.web.multipart.MultipartFile;
  */
 @RestController
 @RequestMapping(path="/api/")
-@CrossOrigin(origins ="*")
+@CrossOrigin(origins = "*")
 public class AnnonceController {
 
 	@Autowired
